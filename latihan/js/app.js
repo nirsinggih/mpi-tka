51d34acf277a085d6b1a6c2679d74ebf95894b6a
Nir Singgih <nirsinggih@gmail.com>
2026-09-15 07:51:51 +0700

    Create app.js

---
+++ latihan/js/app.js
@@ -0,0 +1,44 @@
+document
+    .getElementById("formIdentitas")
+    .addEventListener("submit", function(e) {
+
+        e.preventDefault();
+
+        const nama = document
+            .getElementById("nama")
+            .value
+            .trim();
+
+        const sekolah = document
+            .getElementById("sekolah")
+            .value
+            .trim();
+
+        const kelas = document
+            .getElementById("kelas")
+            .value;
+
+        const nomor = document
+            .getElementById("nomor")
+            .value
+            .trim();
+
+        if (!nama || !sekolah || !kelas) {
+            alert("Mohon lengkapi identitas siswa.");
+            return;
+        }
+
+        const siswa = {
+            nama: nama,
+            sekolah: sekolah,
+            kelas: kelas,
+            nomor: nomor
+        };
+
+        localStorage.setItem(
+            "dataSiswa",
+            JSON.stringify(siswa)
+        );
+
+        window.location.href = "quiz.html";
+    });
